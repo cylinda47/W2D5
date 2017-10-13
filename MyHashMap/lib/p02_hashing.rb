5be6ff83ac534b4c35814e6b6bee55b34e4df4e9
Linda Chan <cylinda0407@hotmail.com>
2017-10-13 15:13:23 -0700

    Complete Phase 2

--- MyHashMap/lib/p02_hashing.rb
+++ MyHashMap/lib/p02_hashing.rb
@@ -4,11 +4,22 @@ end
 
 class Array
   def hash
+    hash_code = 0
+    self.each_with_index { |el, i| hash_code += el.hash * i.hash }
+    hash_code
   end
+
 end
 
 class String
   def hash
+    alphabet = ("a".."z").to_a + ("A".."Z").to_a
+    hash_code = 0
+    self.split("").each_with_index do |el, i|
+      el = alphabet.index(el)
+      hash_code += el.hash * i.hash
+    end
+    hash_code
   end
 end
 
@@ -17,5 +28,11 @@ class Hash
   # Make sure to implement an actual Hash#hash method
   def hash
     0
+    hash_code = 0
+    self.each do |k, v|
+      # if the key sym -> str
+      hash_code += (k.hash + v.hash)
+    end
+    hash_code
   end
 end
